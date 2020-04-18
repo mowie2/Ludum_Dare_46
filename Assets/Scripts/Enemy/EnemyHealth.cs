62831b5b1fe6ce4457833782f8656bd88d846cbf
Mohamed <mhamoudi@avans.nl>
2020-04-18 21:43:49 +0200

    Healthbar for enemies

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnemyHealth : MonoBehaviour
 {
     private readonly float maxHealth = 100;
 
     private float currentHealth;
+    private Slider healthBar;
     void Start()
     {
         currentHealth = maxHealth;
+
+        healthBar = transform.Find("Canvas").Find("Healthbar").GetComponent<Slider>();
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+
+        healthBar.GetComponent<CanvasGroup>().alpha = 0;
+
     }
 
     // Update is called once per frame
@@ -20,12 +28,15 @@ public class EnemyHealth : MonoBehaviour
 
     public void DoDamage(float damageAmount)
     {
+        healthBar.GetComponent<CanvasGroup>().alpha = 1;
         currentHealth -= damageAmount;
+
         if (currentHealth <= 0) Death();
+
+        healthBar.value = currentHealth;
     }
     public void Death()
     {
-
         Instantiate(Resources.Load<GameObject>("Prefabs/battery"), gameObject.transform.position, Quaternion.identity);
 
         Destroy(gameObject);
