a52fff4498bb913442af879d26e7f91b4aeb5b18
Mohamed <mhamoudi@avans.nl>
2020-04-18 20:11:57 +0200

    bullet rotation

--- Assets/Scripts/CharacterShooting.cs
+++ Assets/Scripts/CharacterShooting.cs
@@ -29,13 +29,15 @@ public class CharacterShooting : MonoBehaviour
     private void Shoot(Vector3 mousePosition)
     {
         myCharacterPower.DrainInstantly(10f);
+
         Vector3 currentPositon = gameObject.transform.position;
-
-
         Vector3 direction = mousePosition - currentPositon;
         direction.Normalize();
 
         GameObject bullet =  Instantiate(currentBullet, currentPositon, Quaternion.identity);
         bullet.GetComponent<BulletMovement>().direction = direction;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
