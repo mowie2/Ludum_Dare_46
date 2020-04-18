fd3ea87975cc12b1abeceefa0b4bd080e155ff41
Mohamed <mhamoudi@avans.nl>
2020-04-18 19:40:53 +0200

    Bullet direction

--- Assets/Scripts/CharacterShooting.cs
+++ Assets/Scripts/CharacterShooting.cs
@@ -20,16 +20,22 @@ public class CharacterShooting : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Shoot(direction);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Shoot(mousePosition);
         }
    
     }
 
-    private void Shoot(Vector2 direction)
+    private void Shoot(Vector3 mousePosition)
     {
         myCharacterPower.DrainInstantly(10f);
         Vector3 currentPositon = gameObject.transform.position;
-        Instantiate(currentBullet, currentPositon, Quaternion.identity);
+
+
+        Vector3 direction = mousePosition - currentPositon;
+        direction.Normalize();
+
+        GameObject bullet =  Instantiate(currentBullet, currentPositon, Quaternion.identity);
+        bullet.GetComponent<BulletMovement>().direction = direction;
     }
 }
