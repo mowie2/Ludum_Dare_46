dc0b8e810e9b3ae7c2ff6e30d7f04c6f7d4d77f2
Dan Wilkins <danielwilkins5@gmail.com>
2020-04-19 14:22:01 +0100

    Added Turret Enemy

--- Assets/Scripts/Character/CharacterTurret.cs
+++ Assets/Scripts/Character/CharacterTurret.cs
@@ -2,19 +2,26 @@
 
 public class CharacterTurret : MonoBehaviour
 {
-    private void RotateTurrentToMouse()
+    private Transform character;
+
+    private void RotateTurrentToMouse(Vector3 targetPosition)
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 currentPositon = transform.position;
-        Vector3 direction = mousePosition - currentPositon;
+        Vector3 direction = targetPosition - currentPositon;
         direction.Normalize();
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
+    private void Start()
+    {
+        character = GameObject.Find("Character").transform;
+    }
+
     private void Update()
     {
-        RotateTurrentToMouse();
+        if (transform.CompareTag("Player")) RotateTurrentToMouse(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        if (transform.CompareTag("Enemy")) RotateTurrentToMouse(character.transform.position);
     }
 }
