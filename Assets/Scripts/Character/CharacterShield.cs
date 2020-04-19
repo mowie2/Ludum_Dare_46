e20da5ab536ea0a4c3376a4657bba629e6b7b126
Mohamed <mhamoudi@avans.nl>
2020-04-19 12:30:35 +0200

    started on shield

---
+++ Assets/Scripts/Character/CharacterShield.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterShield : MonoBehaviour
+{
+    CharacterPower myCharacterPower;
+    float shieldPowerDrain;
+    Coroutine powerDrainCoroutine;
+    void Start()
+    {
+        shieldPowerDrain = 6f;
+        myCharacterPower = GetComponent<CharacterPower>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && powerDrainCoroutine == null)
+        {
+            TurnOnShield();
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            myCharacterPower.StopPowerDrain(powerDrainCoroutine);
+            powerDrainCoroutine = null;
+        }
+    }
+
+    private void TurnOnShield()
+    {
+        powerDrainCoroutine = myCharacterPower.DrainOverASecond(shieldPowerDrain);
+    }
+}
